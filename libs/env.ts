18c4d6a7ed6017c9e5ad21b3037e340a1d41a4aa
s24004 <s24004@std.it-college.ac.jp>
2026-03-19 21:16:36 +0900

    環境変数を変更

--- libs/env.ts
+++ libs/env.ts
@@ -1,8 +1,6 @@
 const isDevMode = process.env.DEVMODE === "true";
-const isSaveApi = process.env.SAVE_API === "true";
 
 
 export const ENV = {
     isDevMode,
-    isSaveApi
 }
--- libs/vblob.ts
+++ libs/vblob.ts
@@ -6,12 +6,13 @@ import path from "path";
 
 
 
-const isSaveMode = ENV.isDevMode && process.env.SAVE_API === "true";
 
 const tmp_path = path.join(process.cwd(), "tmp.json");
 const BlobUrls: string[] = [];
 
-if (isSaveMode) {
+
+//開発環境では、API制限対策のため、BlobのURLをローカルに保存しておく
+if (ENV.isDevMode) {
     if (fs.existsSync(tmp_path)) {
         const data = fs.readFileSync(tmp_path, "utf-8");
         const urls = JSON.parse(data) as string[];
@@ -30,11 +31,15 @@ if (isSaveMode) {
 
 export async function GetBlobUrls() {
     if (ENV.isDevMode) {
-        if (process.env.SAVE_API === "true") {
-            return BlobUrls;
-        }
-        console.warn("use blob list api");
-        return (await list()).blobs.map(b => b.downloadUrl);
+        // if (process.env.SAVE_API === "true") {
+        //     return BlobUrls;
+        // }
+        // console.warn("use blob list api");
+        // return (await list()).blobs.map(b => b.downloadUrl);
+
+
+        //api制限対策のため、保存されたURLリストを返す
+        return BlobUrls;
     }
     return [];
 }
@@ -47,13 +52,12 @@ export async function AddBlob(file: File) {
     
     const result = await put(path.join("/image", crypto.randomUUID()), file, {
         access: "public",
-        contentType: file.type
+        contentType: file.type,
+        cacheControlMaxAge: 60 * 60 * 24 * 30 * 3, //3ヶ月キャッシュ
     });
 
-    if (isSaveMode) {
-        BlobUrls.push(result.downloadUrl);
-        fs.writeFileSync(tmp_path, JSON.stringify(BlobUrls), "utf-8");
-    }
+    BlobUrls.push(result.downloadUrl);
+    fs.writeFileSync(tmp_path, JSON.stringify(BlobUrls), "utf-8");
 
     return result;
 }
